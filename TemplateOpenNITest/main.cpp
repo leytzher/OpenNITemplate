c4b694ce47f9e9d495f7795891932d984f575283
leytzher <leytzher@gmail.com>
2015-03-01 15:22:55 +0800

    Script to test if kinect is working

--- TemplateOpenNITest/main.cpp
+++ TemplateOpenNITest/main.cpp
@@ -9,35 +9,48 @@
 #include <iostream>
 #include "OpenNI.h"
 #include "NiTE.h"
-
-
-char ReadLastCharOfLine(void);
-
 using namespace openni;
 using namespace std;
+
+char ReadLastCharOfLine(void);
+bool HandleStatus(Status);
+
+
 int main(int argc, const char * argv[]) {
-    printf("OpenNI Version is %d.%d.%d.%d",
-           OpenNI::getVersion().major,
-           OpenNI::getVersion().minor,
-           OpenNI::getVersion().maintenance,
-           OpenNI::getVersion().build);
-    printf("Scanning machine for devices and loading "
-           "modules/drivers ... \r\n");
+
     Status status = STATUS_OK;
-    
-    status = OpenNI::initialize();
-    if(status!= STATUS_OK){
-        printf("ERROR: %d, %s", status,
-               OpenNI::getExtendedError());
-        return 1;
-    }
-    printf("Completed. \r\n");
-    printf("Press enter to exit.\r\n");
-    ReadLastCharOfLine();
-    OpenNI::shutdown();
-    
-    
-    return 0;
+    printf("Scanning machine for devices and loading " " modules/drivers ...\r\n");
+           status = OpenNI::initialize();
+           if (!HandleStatus(status)) return 1;
+           printf("Completed.\r\n");
+           openni::Array<openni::DeviceInfo> listOfDevices;
+           openni::OpenNI::enumerateDevices(&listOfDevices);
+           int numberOfDevices = listOfDevices.getSize();
+           if (numberOfDevices > 0){
+               printf("%d Device(s) are available to use.\r\n\r\n",
+                      numberOfDevices);
+               for (int i = 0; i < numberOfDevices; i++)
+               {
+                   openni::DeviceInfo device = listOfDevices[i];
+                   printf("%d. %s->%s (VID: %d | PID: %d) is connected "
+                          " at %s\r\n",
+                          i,
+                          device.getVendor(),
+                          device.getName(),
+                          device.getUsbVendorId(),
+                          device.getUsbProductId(),
+                          device.getUri());
+               }
+           }else{
+               printf("No device connected to this machine.");
+           }
+           
+           printf("Press ENTER to exit.\r\n");
+           ReadLastCharOfLine();
+           OpenNI::shutdown();
+           return 0;
+
+
 }
 
 
@@ -50,5 +63,15 @@ char ReadLastCharOfLine()
         newChar = getchar();
     } while((newChar != '\n') && (newChar != EOF));
     return (char) lastChar;
+}
+
+bool HandleStatus(Status status)
+{
+    if (status == STATUS_OK)
+        return true;
+    printf("ERROR: #%d, %s", status,
+           OpenNI::getExtendedError());
+    ReadLastCharOfLine();
+    return false;
 }
 
