3d87a19d0ef540e0b3fac574493eaed203391a77
leytzher <leytzher@gmail.com>
2015-03-01 15:10:19 +0800

    Changing include files from square brackets to quotes

--- TemplateOpenNITest/main.cpp
+++ TemplateOpenNITest/main.cpp
@@ -7,9 +7,48 @@
 //
 
 #include <iostream>
+#include "OpenNI.h"
+#include "NiTE.h"
 
+
+char ReadLastCharOfLine(void);
+
+using namespace openni;
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("OpenNI Version is %d.%d.%d.%d",
+           OpenNI::getVersion().major,
+           OpenNI::getVersion().minor,
+           OpenNI::getVersion().maintenance,
+           OpenNI::getVersion().build);
+    printf("Scanning machine for devices and loading "
+           "modules/drivers ... \r\n");
+    Status status = STATUS_OK;
+    
+    status = OpenNI::initialize();
+    if(status!= STATUS_OK){
+        printf("ERROR: %d, %s", status,
+               OpenNI::getExtendedError());
+        return 1;
+    }
+    printf("Completed. \r\n");
+    printf("Press enter to exit.\r\n");
+    ReadLastCharOfLine();
+    OpenNI::shutdown();
+    
+    
     return 0;
 }
+
+
+char ReadLastCharOfLine()
+{
+    int newChar = 0;
+    int lastChar;
+    do{
+        lastChar = newChar;
+        newChar = getchar();
+    } while((newChar != '\n') && (newChar != EOF));
+    return (char) lastChar;
+}
+
