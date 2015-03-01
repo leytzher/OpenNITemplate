b1bda0b9c3e7120939d197fb9387f24df788e844
leytzher <leytzher@gmail.com>
2015-03-01 14:35:25 +0800

    Initial Commit

---
+++ TemplateOpenNITest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TemplateOpenNITest
+//
+//  Created by Leytzher on 3/1/15.
+//  Copyright (c) 2015 Leytzher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
